2277d2c505a4db8c1a59dbbf37f071d0a70fa8ce
Nakarasan <krishzkara27@gmail.com>
2024-07-27 00:31:58 +0530

    nav constants added

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export interface menuProps {
+  setOpen: any;
+}
+
+export const menuItems = [
+  {
+    name: "Home",
+    to: "/",
+  },
+  {
+    name: "Projects",
+    to: "/projects",
+  },
+  {
+    name: "Contact Us",
+    to: "/contact-us",
+  },
+  {
+    name: "WorkLogs",
+    to: "/workLogs",
+  },
+];
