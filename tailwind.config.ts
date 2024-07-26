605b1a0446739e1b6ffe91e69425224b143d837e
Nakarasan <krishzkara27@gmail.com>
2024-07-27 00:23:03 +0530

    font, text, color config in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,30 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/templates/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "350px",
+      },
+      colors: {
+        bgcolor: "#060021",
+      },
+
+      fontFamily: {
+        poppins: ["PoppinsRegular", "serif"],
+        poppinsMedium: ["PoppinsMedium", "serif"],
+        poppinsSemiBold: ["PoppinsSemiBold", "serif"],
+      },
+
+      fontSize: {
+        h1: "16px",
+        h2: "14px",
+        p1: "13px",
+        p2: "12px",
+        p3: "11px",
       },
     },
   },
