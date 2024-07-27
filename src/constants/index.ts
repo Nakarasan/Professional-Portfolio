90ccb9c0105aa7c9d3323479d289abb2dca90fa5
Nakarasan <krishzkara27@gmail.com>
2024-07-28 00:31:56 +0530

    hire me details added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,13 @@
+//latest project images
 import pro1 from "@/assets/images/projects/scl.svg";
 import pro2 from "@/assets/images/projects/Ecommerce.svg";
 import pro3 from "@/assets/images/projects/speedHRM.svg";
+
+// why hire me images
+import workaholic from "@/assets/images/hire/workaholic.svg";
+import communicative from "@/assets/images/hire/communicative.svg";
+import cooperative from "@/assets/images/hire/cooperative.svg";
+import perfectionist from "@/assets/images/hire/perfectionist.svg";
 
 export interface menuProps {
   setOpen: any;
@@ -39,5 +46,33 @@ export const latestProjects = [
   {
     name: "SpeedHrm",
     image: pro3,
+  },
+];
+
+//why hire me constants
+export const hireMeDetails = [
+  {
+    attitude: "Workaholic",
+    image: workaholic,
+    description:
+      "I’m a kind of person who can’t just stand around and doing nothing. I have a tendency to do something productive.",
+  },
+  {
+    attitude: "Communicative",
+    image: communicative,
+    description:
+      "I have a broad understanding of verbal vocabulary. Therefore, I can convey a message well to the receiver.",
+  },
+  {
+    attitude: "Cooperative",
+    image: cooperative,
+    description:
+      "Behind the successful project, there’s a great team. I can build a good cooperation and remain consistent with the goal.",
+  },
+  {
+    attitude: "Perfectionist",
+    image: perfectionist,
+    description:
+      "I have a strong intuition. I have remained consistent with high quality standards to present a most worthy result.",
   },
 ];
