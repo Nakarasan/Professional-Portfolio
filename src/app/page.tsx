2933b83239d4da07b49f99b2fbc9c68ed0c1ec31
Nakarasan <krishzkara27@gmail.com>
2024-07-27 20:20:07 +0530

    latest project component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Profile from "@/templates/home/Profile";
+import { LatestProjects } from "@/templates/home";
+import { Profile } from "@/templates/home/Profile";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-10">
       <Profile />
+      <LatestProjects />
     </div>
   );
 }
