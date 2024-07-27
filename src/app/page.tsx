239eabed82bd1e1001febcfff178732d3054c2e9
Nakarasan <krishzkara27@gmail.com>
2024-07-28 00:31:33 +0530

    why hire me added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { LatestProjects } from "@/templates/home";
 import { Profile } from "@/templates/home/Profile";
-import Image from "next/image";
+import WhyHireMe from "@/templates/home/WhyHireMe";
 
 export default function Home() {
   return (
     <div className="space-y-10">
       <Profile />
       <LatestProjects />
+      <WhyHireMe />
     </div>
   );
 }
