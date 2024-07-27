e29ec6e7637246c236ceadd9951c4e9cba7c5a31
Nakarasan <krishzkara27@gmail.com>
2024-07-27 20:21:00 +0530

    latestProject constants added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,12 @@
+import pro1 from "@/assets/images/projects/scl.svg";
+import pro2 from "@/assets/images/projects/Ecommerce.svg";
+import pro3 from "@/assets/images/projects/speedHRM.svg";
+
 export interface menuProps {
   setOpen: any;
 }
 
+// header nav items
 export const menuItems = [
   {
     name: "Home",
@@ -12,11 +17,27 @@ export const menuItems = [
     to: "/projects",
   },
   {
+    name: "WorkLogs",
+    to: "/workLogs",
+  },
+  {
     name: "Contact Us",
     to: "/contact-us",
   },
+];
+
+// latest project list
+export const latestProjects = [
   {
-    name: "WorkLogs",
-    to: "/workLogs",
+    name: "Speedcodelabs - website",
+    image: pro1,
+  },
+  {
+    name: "E-Commerce",
+    image: pro2,
+  },
+  {
+    name: "SpeedHrm",
+    image: pro3,
   },
 ];
